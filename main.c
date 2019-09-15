d89212dd8a37282493ebdab0278fa85db4d3642b
Peter McMaster <pmcmaster9090@gmail.com>
2019-09-14 22:08:15 -0400

    Created defaults header

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include <getopt.h>
 #include <alsa/asoundlib.h>
 
-#define DEFAULT_CHANNELS 2
-#define DEFAULT_SAMPLE_RATE 48000
-#define DEFAULT_BITDEPTH SND_PCM_FORMAT_S16_LE 
-#define DEFAULT_PERIOD_SIZE 1024;
+#include "defaults.h"
 
 typedef struct {
 	unsigned int samplerate;
