76a50e4026ccbf13b7f10d65d20efbdfd7a624fc
Peter McMaster <pmcmaster9090@gmail.com>
2019-09-16 09:45:04 -0400

    Polishing

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
+/* See LICENSE for license details.
+ * Developed by Peter McMaster.
+ * Audio tool used to capture, or playback raw audio files.
+ */
+
 #include <getopt.h>
 #include <alsa/asoundlib.h>
 
 #include "defaults.h"
 
+/* Stores pcm information */
 typedef struct {
 	unsigned int samplerate;
 	snd_pcm_uframes_t periodsize;
@@ -16,38 +22,109 @@ typedef struct {
 
 /* Prints out alsa hw params. */
 void alsa_info(SignalConfig *sc);
-/* Reads from the hw buffer and writes to stdout. */
-int capture_audio(SignalConfig *sc, char *buf, int size);
 /* Populates the alsa hw params struct. */
 int configure_alsa(SignalConfig *sc);
 /* Fills a new signal_config struct with DEFINE values. */
 SignalConfig *init_signal_config();
-/* Sets values in signal config struct with values from argv. */
-int parse_args(SignalConfig *sc, int argc, char* argv[]);
+/* prints error_string and uses snd_strerror() to resolve err to a alsa error string. */
+void print_error(char *error_string, int err);
+
+/* Reads from the hw buffer and writes to stdout. */
+int capture_audio(SignalConfig *sc, char *buf, int size);
 /* Reads values from stdin and writes to hardware buffer. */
 int playback_audio(SignalConfig *sc, char *buf, int size);
+
+/* Sets values in signal config struct with values from argv. */
+int parse_args(SignalConfig *sc, int argc, char* argv[]);
 /* Converts the arg passed to a pcm bit format type. */
 snd_pcm_format_t search_pcmformat_from_argbitformat(char *arg);
 
-void alsa_info(SignalConfig *sc)
+void 
+alsa_info(SignalConfig *sc)
 {
 	snd_pcm_hw_params_get_rate(sc->hwparams,&(sc->samplerate),&(sc->dir));
-	printf("Sample rate: %d bps\n",sc->samplerate);
+	printf("Sample rate(bps): %d\n", sc->samplerate);
 
 	snd_pcm_hw_params_get_periods(sc->hwparams,&(sc->samplerate), &(sc->dir));
-	printf("Periods = %d\n",sc->samplerate);
+	printf("Periods: %d\n", sc->samplerate);
 
 	snd_pcm_hw_params_get_period_time(sc->hwparams,&(sc->samplerate),&(sc->dir));
-	printf("Period time =  %d microseconds\n",sc->samplerate);
+	printf("Period time(microseconds): %d\n", sc->samplerate);
 
   	snd_pcm_hw_params_get_period_size(sc->hwparams,&(sc->periodsize), &(sc->dir));
-  	printf("Period size = %d frames\n", (int)sc->periodsize);
+  	printf("Period size(frames): %d\n", (int)sc->periodsize);
 	
 	snd_pcm_hw_params_get_buffer_size(sc->hwparams,(snd_pcm_uframes_t *)&(sc->samplerate));
-	printf("Buffer Size = %d frames\n",sc->samplerate);
+	printf("Buffer Size(frames): %d\n", sc->samplerate);
 }
 
-int capture_audio(SignalConfig *sc, char *buf, int size)
+int 
+configure_alsa(SignalConfig *sc)
+{
+	int err;
+	if((err = snd_pcm_open(&(sc->pcmhandle), "default", sc->pcmdir, 0)) < 0) {
+		print_error("Error opening audio device",err);
+		return -1;
+	}
+
+	snd_pcm_hw_params_alloca(&(sc->hwparams));
+
+	if((err = snd_pcm_hw_params_any(sc->pcmhandle,sc->hwparams)) < 0) {
+		print_error("Error setting default hw params",err);
+		return -1;
+	}
+
+	if((err = snd_pcm_hw_params_set_access(sc->pcmhandle,sc->hwparams,SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+		print_error("Error setting access mode to interleaved",err);
+		return -1;
+	}
+
+	if((err = snd_pcm_hw_params_set_format(sc->pcmhandle,sc->hwparams,sc->bitdepth)) < 0) {
+		print_error("Error setting bit format",err);
+		return -1;
+	}
+	
+	if((err = snd_pcm_hw_params_set_channels(sc->pcmhandle,sc->hwparams,sc->channels)) < 0) {
+		print_error("Error setting channels",err);
+		return -1;
+	}
+
+	if((err = snd_pcm_hw_params_set_rate_near(sc->pcmhandle, sc->hwparams, &(sc->samplerate), &(sc->dir))) < 0) {
+		print_error("Error setting rate",err);
+		return -1;
+	}
+	
+	if((err = snd_pcm_hw_params_set_period_size_near(sc->pcmhandle, sc->hwparams, &(sc->periodsize), &(sc->dir))) < 0) {
+		print_error("Error setting period size",err);
+		return -1;
+	}
+
+	if((err = snd_pcm_hw_params(sc->pcmhandle,sc->hwparams)) < 0) {
+		print_error("Error applying pcmhandle to hw params",err);
+		return -1;
+	}
+	return 0;
+}
+
+SignalConfig *
+init_signal_config()
+{
+	SignalConfig *sc = malloc(sizeof(SignalConfig));
+	sc->samplerate=DEFAULT_SAMPLE_RATE;
+	sc->periodsize=DEFAULT_PERIOD_SIZE;
+	sc->channels=DEFAULT_CHANNELS;
+	sc->bitdepth=DEFAULT_BITDEPTH;
+	return sc;
+}
+
+void 
+print_error(char *error_string, int err)
+{
+	fprintf(stderr,"%s\n%s\n",error_string,snd_strerror(err));
+}
+
+int 
+capture_audio(SignalConfig *sc, char *buf, int size)
 {
 	int err;
 	while(1) {
@@ -68,52 +145,35 @@ int capture_audio(SignalConfig *sc, char *buf, int size)
 	return 0;
 }
 
-int configure_alsa(SignalConfig *sc)
+int 
+playback_audio(SignalConfig *sc, char *buf, int size)
 {
 	int err;
-	if((err = snd_pcm_open(&(sc->pcmhandle), "default", sc->pcmdir, 0)) < 0) {
-		fprintf(stderr,"Error opening audio device %s\n",snd_strerror(err));
-		return -1;
-	}
+	while((err = read(0,buf,size)) != 0) {
+		if(err == 0) {
+			fprintf(stderr, "end of file on input\n");
+			break;
+			printf("what the fuck\n");
+		} else if(err != size) {
+			fprintf(stderr,"short read: read %d bytes\n",err);
+		}
 
-	snd_pcm_hw_params_alloca(&(sc->hwparams));
-
-	if((err = snd_pcm_hw_params_any(sc->pcmhandle,sc->hwparams)) < 0) {
-		fprintf(stderr,"Error setting default hw params\n");
-		return -1;
-	}
-
-	if((err = snd_pcm_hw_params_set_access(sc->pcmhandle,sc->hwparams,SND_PCM_ACCESS_RW_INTERLEAVED))) {
-		return -1;
-		fprintf(stderr,"Error setting access mode to interleaved\n");
-	}
-
-	snd_pcm_hw_params_set_format(sc->pcmhandle,sc->hwparams,sc->bitdepth);
-	
-	snd_pcm_hw_params_set_channels(sc->pcmhandle,sc->hwparams,sc->channels);
-	
-	snd_pcm_hw_params_set_rate_near(sc->pcmhandle, sc->hwparams, &(sc->samplerate), &(sc->dir));
-	
-	snd_pcm_hw_params_set_period_size_near(sc->pcmhandle, sc->hwparams, &(sc->periodsize), &(sc->dir));
-
-	if((err = snd_pcm_hw_params(sc->pcmhandle,sc->hwparams)) < 0) {
-		fprintf(stderr,"Cannot set hw params\n");
-		return -1;
+		err = snd_pcm_writei(sc->pcmhandle,buf,sc->periodsize);
+		if(err == -EPIPE) {
+			fprintf(stderr,"underrun occurred\n");
+			snd_pcm_prepare(sc->pcmhandle);
+			return -1;
+		} else if(err < 0) {
+			fprintf(stderr,"error from writei: %s\n",snd_strerror(err));
+		} else if(err != (int)sc->periodsize) {
+			fprintf(stderr,"Short write: written %d frames\n",err);
+		}
 	}
 	return 0;
 }
 
-SignalConfig *init_signal_config()
-{
-	SignalConfig *sc = malloc(sizeof(SignalConfig));
-	sc->samplerate=DEFAULT_SAMPLE_RATE;
-	sc->periodsize=DEFAULT_PERIOD_SIZE;
-	sc->channels=DEFAULT_CHANNELS;
-	sc->bitdepth=DEFAULT_BITDEPTH;
-	return sc;
-}
-
-int parse_args(SignalConfig *sc, int argc, char* argv[])
+int 
+parse_args(SignalConfig *sc, int argc, char* argv[])
 {
 	struct option options[] = {
 		{"r",1,0,'r'},
@@ -160,33 +220,8 @@ int parse_args(SignalConfig *sc, int argc, char* argv[])
 	return 0;
 }
 
-int playback_audio(SignalConfig *sc, char *buf, int size)
-{
-	int err;
-	while((err = read(0,buf,size)) != 0) {
-		if(err == 0) {
-			fprintf(stderr, "end of file on input\n");
-			break;
-			printf("what the fuck\n");
-		} else if(err != size) {
-			fprintf(stderr,"short read: read %d bytes\n",err);
-		}
-
-		err = snd_pcm_writei(sc->pcmhandle,buf,sc->periodsize);
-		if(err == -EPIPE) {
-			fprintf(stderr,"underrun occurred\n");
-			snd_pcm_prepare(sc->pcmhandle);
-			return -1;
-		} else if(err < 0) {
-			fprintf(stderr,"error from writei: %s\n",snd_strerror(err));
-		} else if(err != (int)sc->periodsize) {
-			fprintf(stderr,"Short write: written %d frames\n",err);
-		}
-	}
-	return 0;
-}
-
-snd_pcm_format_t search_pcmformat_from_argbitformat(char *arg)
+snd_pcm_format_t 
+search_pcmformat_from_argbitformat(char *arg)
 {
 	struct bitformat{
 		snd_pcm_format_t sndvalue;
@@ -208,7 +243,8 @@ snd_pcm_format_t search_pcmformat_from_argbitformat(char *arg)
 	return -1;
 }
 
-int main(int argc, char* argv[])
+int 
+main(int argc, char* argv[])
 {
 	SignalConfig *sc;		
 	int err;
@@ -216,7 +252,9 @@ int main(int argc, char* argv[])
 
 	sc = init_signal_config();
 	parse_args(sc,argc,argv);
-	configure_alsa(sc);
+	if(configure_alsa(sc) < 0) {
+		exit(1);
+	}
 
 #ifdef DEBUG
 	alsa_info(sc);
